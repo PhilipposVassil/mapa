82cca0c556915aa468d78eae8934d793c470ee88
Philippos Vassiliou <philippos.vassiliou@hotmail.com>
2016-07-10 01:18:39 +0300

    Connection with Database

---
+++ admin.php
@@ -0,0 +1,12 @@
+<?php
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$dbname="test";
+	
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password) or die("Unable to connect to MySQL");
+
+	//Select Database
+	$selected = mysqli_select_db($conn,$dbname) or die("Could not select $dbname");
+?>
