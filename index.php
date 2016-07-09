554c998e49843651fe2ca648c2eda3d3b13e338f
Philippos Vassiliou <philippos.vassiliou@hotmail.com>
2016-07-10 01:11:57 +0300

    football frame

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+	<div class="table" align="center">
+		<table border="1" width="350px" align="center">
+			<tr align="center">
+				<td colspan="7"><b>ΑΓΟΝΕΣ ΠΟΔΟΣΦΑΙΡΟΥ</b></td>
+			</tr>
+			<tr>
+				<td colspan="7">&nbsp;</td>
+			</tr>
+			<tr align="center">
+				<td><b>&nbsp;</b></td>
+				<td><b>Ομάδα</b></td>
+				<td><b>Βαθμοί</b></td>
+				<td><b>Αγώνες</b></td>
+				<td><b>Νίκες</b></td>
+				<td><b>Ισοπαλίες</b></td>
+				<td><b>Ήττες</b></td>
+			</tr>
+			<tr>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+			</tr>
+			<tr>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+			</tr>
+            <tr>
+                <td align="center"></td>
+                <td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+            </tr>
+            <tr>
+                <td align="center"></td>
+                <td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+				<td align="center"></td>
+            </tr>
+		</table>
+	</div>
+</html>
